73353e9437da944056d6d9ed3faeab7dcc1cf146
Moirai7 <moirai.zhang@gmail.com>
2017-05-09 19:01:09 +0800

    update

--- lianjia/spiders/ershouSpider.py
+++ lianjia/spiders/ershouSpider.py
@@ -1,8 +1,10 @@
-from scrapy.spiders import Spider
 import json
+import re
+from scrapy.spiders import Spider
 from lianjia.items import LianjiaErshouItem
 from scrapy.selector import Selector
 from scrapy.http import Request
+from lxml import etree
 
 class ershouSpider(Spider):
 	name = "ershou"
@@ -17,7 +19,8 @@ class ershouSpider(Spider):
 		urls = res.xpath("//div[@class='title']/a/@href").extract()
 		for url in urls:
 			print url
-			yield Request(url=url, callback=self.parse_details)
+			#yield Request(url=url, callback=self.parse_details)
+			self.parse_details(url)
 		data = res.xpath("//div[@class='page-box house-lst-page-box']/@page-data").extract()
 		data = json.loads(data)
 		cur = data['curPage']
@@ -25,7 +28,17 @@ class ershouSpider(Spider):
 		if cur <= total:
 			yield Request(url='http://bj.lianjia.com/ershoufang/pg'+str(cur+1),callback=self.parse)
 
-	def parse_details(self,response):
+	def parse_details(self,url):
+		p = requests.get(url)
+		contents = etree.HTML(p.content.decode('utf-8'))
+		latitude = contents.xpath('/ html / body / script[19]/text()').pop()
+		time.sleep(3)
+		regex = '''resblockPosition(.+)'''
+		items = re.search(regex, latitude)
+		content = items.group()[:-1]  
+		longitude_latitude = content.split(':')[1]
+
 		item = LianjiaErshouItem()
-		item['url']=response.url
+		item['url']=p.url
+		item['latitude']=longitude_latitude[1:-1]
 		yield item
